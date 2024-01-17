da3c423a48157248887c4b09d129ecea1783f0c5
Samuel <samuelrivba@gmail.com>
2024-01-16 19:35:36 -0500

    Add player selection

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+const playerScoreEl = document.getElementById('player-score')
+const playerChoiceEl = document.getElementById('player-choice')
+const computerScoreEl = document.getElementById('computer-score')
+const computerChoiceEl = document.getElementById('computer-choice')
+const resulttext = document.getElementById('resultText')
 
+const playerRock = document.getElementById('playerRock')
+const playerPaper = document.getElementById('playerPaper')
+const playerScissors = document.getElementById('playerScissors')
+const playerLizard = document.getElementById('playerLizard')
+const playerSpock = document.getElementById('playerSpock')
+
+const computerRock = document.getElementById('computerRock')
+const computerPaper = document.getElementById('computerPaper')
+const computerScissors = document.getElementById('computerScissors')
+const computerLizard = document.getElementById('computerLizard')
+const computerSpock = document.getElementById('computerSpock')
+
+const allGameIcons = document.querySelectorAll('.far')
 
 const choices = {
   rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
@@ -8,3 +26,37 @@ const choices = {
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
 
+function resetSelected() {
+  allGameIcons.forEach((icon) => {
+    icon.classList.remove('selected')
+  })
+}
+
+function select(playerChoice) {
+  resetSelected()
+  switch(playerChoice) {
+    case 'rock':
+      playerRock.classList.add('selected')
+      playerChoiceEl.textContent = ' --- Rock'
+      break
+    case 'paper':
+      playerPaper.classList.add('selected')
+      playerChoiceEl.textContent = ' --- Paper'
+      break
+    case 'scissors':
+      playerScissors.classList.add('selected')
+      playerChoiceEl.textContent = ' --- Scissors'
+      break
+    case 'lizard':
+      playerLizard.classList.add('selected')
+      playerChoiceEl.textContent = ' --- Lizard'
+      break
+    case 'spock':
+      playerSpock.classList.add('selected')
+      playerChoiceEl.textContent = ' --- Spock'
+      break
+    default:
+      break
+  }
+  console.log(playerChoice)
+}
