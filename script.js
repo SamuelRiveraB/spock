44e5e6b80304eadf536964fc9a50d9f852a4b906
Samuel <samuelrivba@gmail.com>
2024-01-16 20:06:30 -0500

    Game logic

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const playerScoreEl = document.getElementById('player-score')
 const playerChoiceEl = document.getElementById('player-choice')
 const computerScoreEl = document.getElementById('computer-score')
 const computerChoiceEl = document.getElementById('computer-choice')
-const resulttext = document.getElementById('resultText')
+const resultText = document.getElementById('result-text')
 
 const playerRock = document.getElementById('playerRock')
 const playerPaper = document.getElementById('playerPaper')
@@ -26,14 +26,84 @@ const choices = {
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
 
+let computerChoice = ''
+let playerScoreNumber = 0
+let computerScoreNumber = 0
+
 function resetSelected() {
   allGameIcons.forEach((icon) => {
     icon.classList.remove('selected')
   })
 }
 
-function select(playerChoice) {
+function computerRandomChoice() {
+  const computerChoiceNumber = Math.random()
+  if (computerChoiceNumber < 0.2) {
+    computerChoice = 'rock'
+  } else if (computerChoiceNumber <= 0.4) {
+    computerChoice = 'paper'
+  } else if (computerChoiceNumber <= 0.6) {
+    computerChoice = 'scissors'
+  } else if (computerChoiceNumber <= 0.8) {
+    computerChoice = 'lizard'
+  } else if (computerChoiceNumber <= 1) {
+    computerChoice = 'spock'
+  }
+}
+
+function displayComputerChoice() {
+  switch(computerChoice) {
+    case 'rock':
+      computerRock.classList.add('selected')
+      computerChoiceEl.textContent = ' --- Rock'
+      break
+    case 'paper':
+      computerPaper.classList.add('selected')
+      computerChoiceEl.textContent = ' --- Paper'
+      break
+    case 'scissors':
+      computerScissors.classList.add('selected')
+      computerChoiceEl.textContent = ' --- Scissors'
+      break
+    case 'lizard':
+      computerLizard.classList.add('selected')
+      computerChoiceEl.textContent = ' --- Lizard'
+      break
+    case 'spock':
+      computerSpock.classList.add('selected')
+      computerChoiceEl.textContent = ' --- Spock'
+      break
+    default:
+      break
+  }
+}
+
+function updateScore(playerChoice) {
+  if(playerChoice === computerChoice) {
+    resultText.textContent = "It's a tie!"
+  } else {
+    const choice = choices[playerChoice]
+    if (choice.defeats.indexOf(computerChoice) > -1) {
+      resultText.textContent = 'You Won!'
+      playerScoreNumber ++
+      playerScoreEl.textContent = playerScoreNumber
+    } else {
+      resultText.textContent = 'You Lost!'
+      computerScoreNumber ++
+      computerScoreEl.textContent = computerScoreNumber
+    }
+  }
+}
+
+function checkResult(playerChoice) {
   resetSelected()
+  computerRandomChoice()
+  displayComputerChoice()
+  updateScore(playerChoice)
+}
+
+function select(playerChoice) {
+  checkResult(playerChoice)
   switch(playerChoice) {
     case 'rock':
       playerRock.classList.add('selected')
@@ -58,5 +128,4 @@ function select(playerChoice) {
     default:
       break
   }
-  console.log(playerChoice)
 }
